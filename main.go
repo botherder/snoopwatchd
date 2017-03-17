40da3d7c330fc4dfa134c008365e5e2efa463d70
Nex <nex@nex.sx>
2017-03-17 15:41:23 +0100

    Added some comments

--- main.go
+++ main.go
@@ -16,8 +16,8 @@ import (
 )
 
 func get_cams() ([]string) {
+    // Get list of files in /dev/.
     var dev string = "/dev/"
-
     files, err := ioutil.ReadDir(dev)
     if err != nil {
         log.Fatal(err)
@@ -25,6 +25,7 @@ func get_cams() ([]string) {
 
     devices := []string{}
     for _, f := range files {
+        // Check if a file starts with "video", which should be at webcam.
         if !f.IsDir() && strings.HasPrefix(f.Name(), "video") {
             device := filepath.Join(dev, f.Name())
             devices = append(devices, device)
@@ -35,18 +36,22 @@ func get_cams() ([]string) {
 }
 
 func get_mics() ([]string) {
+    // Read file containing details on audio devices.
     devices_content, _ := ioutil.ReadFile("/proc/asound/devices")
 
+    // Look for those marked as recording devices.
     re := regexp.MustCompile(`(?m)^.*\[(.*?)\].*capture`)
     matches := re.FindAllStringSubmatch(string(devices_content), -1)
 
     devices := []string{}
     for _, match := range matches {
+        // For each entry we identify the appropriate device.
         identifiers := strings.Split(match[1], "-")
         card_id := strings.TrimSpace(identifiers[0])
         device_id := strings.TrimSpace(identifiers[1])
         device_path := fmt.Sprintf("/dev/snd/pcmC%sD%sc", card_id, device_id)
         
+        // Check if device name exists, if so add it to the list.
         if _, err := os.Stat(device_path); err == nil {
             devices = append(devices, device_path)
         }
@@ -56,6 +61,7 @@ func get_mics() ([]string) {
 }
 
 func watch_for_events(device string) {
+    // Initialize syslog logger for this watcher.
     slog, err := syslog.New(syslog.LOG_ERR, "snoopwatchd")
 
     watcher, err := fsnotify.NewWatcher()
@@ -64,20 +70,28 @@ func watch_for_events(device string) {
     }
     defer watcher.Close()
 
+    // Add device to the watcher.
     err = watcher.Add(device)
     if err != nil {
         log.Println(err)
     }
 
+    // Watch for events.
     for {
         select {
         case event := <-watcher.Events:
+            // If it's an IN_OPEN event the current device as accessed.
             if event.Op&fsnotify.Open == fsnotify.Open {
                 msg := fmt.Sprintf("Some process accessed device %s", event.Name)
 
+                // Try to find processes that have an open handle to the device.
+                // WARNING: This isn't actually directly connected to this
+                // particular event. We assume there might be only one process
+                // with access to the device at any given time.
                 current_pids, err := psfiles.FindProcsByFile(event.Name)
                 if err == nil {
                     for _, pid := range current_pids {
+                        // Get details on the process.
                         proc, _ := psprocess.NewProcess(int32(pid))
                         proc_exe, _ := proc.Exe()
                         proc_cmd, _ := proc.Cmdline()
@@ -86,8 +100,10 @@ func watch_for_events(device string) {
                     }
                 }
 
+                // Log to syslog and to console.
                 slog.Alert(msg)
                 log.Println(msg)
+            // If it's an IN_DELETE file, we assume the device was disconnected.
             } else if event.Op&fsnotify.Remove == fsnotify.Remove {
                 return
             }
@@ -97,6 +113,7 @@ func watch_for_events(device string) {
     }
 }
 
+// Spaghetti code to remove a device from a list.
 func remove_from_slice(things []string, item string) []string {
     var to_remove int
     for index, thing := range things {
@@ -110,6 +127,7 @@ func remove_from_slice(things []string, item string) []string {
 }
 
 func main() {
+    // Initialize syslog logger for the core procedure.
     slog, err := syslog.New(syslog.LOG_ERR, "snoopwatchd")
     if err != nil {
         log.Fatal(err)
@@ -118,9 +136,12 @@ func main() {
 
     monitored := []string{}
     for {
+        // Fetch currently available webcams and microphones.
         devices := append(get_cams(), get_mics()...)
         for _, device := range devices {
             var found bool = false
+            // Loop through current devices and check if they have already
+            // been marked for monitoring.
             for _, monitor := range monitored {
                 if device == monitor {
                     found = true
@@ -128,6 +149,8 @@ func main() {
                 }
             }
 
+            // If the device does not appear in the monitored list, add it
+            // and start the related watcher.
             if found == false {
                 msg := fmt.Sprintf("New device was connected %s", device)
                 slog.Info(msg)
@@ -138,6 +161,7 @@ func main() {
             }
         }
 
+        // Now we check if any device was removed.
         new_monitored := monitored
         for _, monitor := range monitored {
             var found bool = false
@@ -148,6 +172,8 @@ func main() {
                 }
             }
 
+            // If the device in the monitored list isn't currently available
+            // we remove it from the list.
             if found == false {
                 msg := fmt.Sprintf("Device was disconnected %s", monitor)
                 slog.Alert(msg)
